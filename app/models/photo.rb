6d70403237c4612d8b1f83fa164e696b28167882
Antonia Cronin <antoniajcronin@gmail.com>
2018-02-25 15:55:31 +0000

    first commit includes peprclip

---
+++ app/models/photo.rb
@@ -0,0 +1,9 @@
+class Photo < ApplicationRecord
+  #Mounts paperclip image
+  has_attached_file :image
+  #This validates the type of file uploaded. According to this, only images are allowed.
+  #Validates file, file type and file size
+  validates_attachment :image, presence: true,
+  content_type: { content_type: "image/jpeg" },
+  size: { in: 0..1000.kilobytes }
+end
